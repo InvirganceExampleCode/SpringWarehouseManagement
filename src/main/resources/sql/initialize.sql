aed94bfc8ca7d19c8bce457bd6d9799e68d9c9e9
jbanes <jbanes@gmail.com>
2025-02-19 16:52:10 -0600

    Bug fixes in init script

--- src/main/resources/sql/initialize.sql
+++ src/main/resources/sql/initialize.sql
@@ -2,13 +2,13 @@ create table if not exists tbl_barang (
     id int primary key,
     berat int not null,
     kategori varchar(255),
-    name varchar(255) not null
+    nama varchar(255) not null
 );
 
 create table if not exists tbl_shipper (
     id int primary key,
     code_shipper varchar(255) not null,
-    name varchar(255) not null
+    nama varchar(255) not null
 );
 
 create table if not exists tbl_ship (
@@ -16,7 +16,7 @@ create table if not exists tbl_ship (
     barang_id int not null references tbl_barang,
     shipper_id int not null references tbl_shipper,
     stok int not null,
-    date timestamp,
+    tanggal timestamp,
     tipe varchar(255)
 );
 
@@ -28,8 +28,8 @@ create table if not exists tbl_stok (
 
 create sequence if not exists hibernate_sequence start with 5;
 
-delete from tbl_barang where id in (1, 2);
 delete from tbl_stok where barang_id in (1, 2);
+delete from tbl_barang where id in (1, 2);
 
 insert into tbl_barang values (1, 1, 'CLOTHING', 'Children''s Outfit');
 insert into tbl_barang values (2, 2, 'APPLIANCES', 'Iron');
