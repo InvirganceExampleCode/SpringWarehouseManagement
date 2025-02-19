fbc837d3d269748626d4604f53c8a52d13ceb067
jbanes <jbanes@gmail.com>
2025-02-19 16:20:33 -0600

    Remvoed commented code and updated POM

--- src/main/java/com/warehouse/management/barang/ProductApplication.java
+++ src/main/java/com/warehouse/management/barang/ProductApplication.java
@@ -23,38 +23,4 @@ public class ProductApplication
             service.update(new Query(new ClasspathSource("/sql/initialize.sql")));
         };
     }
-
-//    @Bean
-//    CommandLineRunner commandLineRunner(BarangRepository barangRepository, StockRepository stockRepository) 
-//    {
-//        return (args) -> {
-//            
-//            Barang barang1 = Barang.builder()
-//                            .id(1l)
-//                            .nama("Pakaian Anak")
-//                            .berat(1)
-//                            .kategori(TipeBarangConstant.TIPE_PAKAIAN)
-//                            .build();
-//
-//            Barang barang2 = Barang.builder()
-//                            .id(2l)
-//                            .nama("Setrika")
-//                            .berat(2)
-//                            .kategori(TipeBarangConstant.TIPE_ELEKTRONIK)
-//                            .build();
-//
-//            Stock stock1 = Stock.builder()
-//                            .barangId(1l)
-//                            .stok(0)
-//                            .build();
-//
-//            Stock stock2 = Stock.builder()
-//                            .barangId(2l)
-//                            .stok(0)
-//                            .build();
-//
-//            barangRepository.saveAll(List.of(barang1, barang2));
-//            stockRepository.saveAll(List.of(stock1, stock2));
-//        };
-//    }
 }
