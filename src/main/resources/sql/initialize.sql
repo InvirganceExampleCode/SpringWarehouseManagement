70f578f026b41debbeeddc18849b1d025549c641
jbanes <jbanes@gmail.com>
2025-02-19 16:13:23 -0600

    Missing initialize commands

--- src/main/resources/sql/initialize.sql
+++ src/main/resources/sql/initialize.sql
@@ -14,7 +14,7 @@ create table if not exists tbl_shipper (
 create table if not exists tbl_ship (
     id int primary key,
     barang_id int not null references tbl_barang,
-    shipper_id int not null,
+    shipper_id int not null references tbl_shipper,
     stok int not null,
     date timestamp,
     tipe varchar(255)
@@ -27,3 +27,13 @@ create table if not exists tbl_stok (
 );
 
 create sequence if not exists hibernate_sequence;
+
+delete from tbl_barang where id in (1, 2);
+delete from tbl_stok where barang_id in (1, 2);
+
+insert into tbl_barang values (1, 1, 'CLOTHING', 'Children''s Outfit');
+insert into tbl_barang values (2, 2, 'APPLIANCES', 'Iron');
+
+insert into tbl_stok values (3, 1, 0);
+insert into tbl_stok values (4, 2, 0);
+
